51db79434c235926041c6e1567c456dc5c63cb53
Newland-A <ajn252@gmail.com>
2020-10-06 12:43:18 -0400

    add edit new signup and login.erb files also create controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,14 +5,17 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
-    # enable :sessions
-    # set :sessions, true
-    # set :session_secret, "password_security"
+    enable :sessions
+    set :sessions, true
+    set :session_secret, "password_security"
   end
 
   get "/" do
     erb :landing
   end
 
+  get '/login' do
+    erb :'sessions/login'
+  end
   
 end
